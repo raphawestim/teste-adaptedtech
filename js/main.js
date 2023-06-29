31ab0037b15da11c045f8288986b619a64f2b3eb
Raphael <raphawestim45@gmail.com>
2023-06-29 06:44:41 -0300

    ajustes

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
-var swiper = new Swiper('.swiper-container', {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  scrollbar: {
-    el: '.swiper-scrollbar'
-  }
-});
+document.addEventListener('DOMContentLoaded', function(){
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar'
+    }
+  });
+})
+
+
 
 
 // var swiperOnepage = new Swiper('.swiper-container-onePage', {
