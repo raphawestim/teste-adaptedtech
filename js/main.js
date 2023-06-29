e20c18c97a7c75f7cabf7df2fdbbdbf3901edf60
Raphael <raphawestim45@gmail.com>
2023-06-28 21:09:36 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,50 @@
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar'
+  }
+});
+
+
+// var swiperOnepage = new Swiper('.swiper-container-onePage', {
+//   slidesPerView: 1,
+//   spaceBetween: 10,
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   scrollbar: {
+//     el: '.swiper-scrollbar'
+//   }
+// });
+
+// swiper.on('slideChange', function() {
+//   var activeIndex = swiper.activeIndex;
+
+//   swiper.slides.forEach(function(slide, index) {
+//     if (index === activeIndex) {
+//       slide.classList.remove('swiper-slide-onePage-inativo');
+//     } else {
+//       slide.classList.add('swiper-slide-onePage-inativo');
+//     }
+//   });
+// });
+// var carousel = document.getElementById('carouselExampleIndicators');
+// carousel.addEventListener('slid.bs.carousel', function (event) {
+//   var activeSlideIndex = event.to + 1;
+//   var tagSlider = document.querySelector('.tag-slider');
+//   tagSlider.textContent = activeSlideIndex;
+// });
